f84e77efc8afeeb5ebbe9faa12a5083512bc9a60
NiLeshSPatil777 <mr.patilnilesh777@gmail.com>
2017-12-03 04:16:11 +0530

    Added recipe list

--- shoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoppingApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-   recipes=[];
+   recipes:Recipe[]=[
+     new Recipe('Dhokla','This is gujrati dish','https://www.ndtv.com/cooks/images/dhokla.2.jpg'),
+     new Recipe('Gulab Jamun','This is sweet dish','https://www.inhouserecipes.com/wp-content/uploads/2013/05/gulab-jamun-recipe.jpg',
+       )
+   ];
   constructor() { }
 
   ngOnInit() {
